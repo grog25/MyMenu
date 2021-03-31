ede888d7909715291a53f573120a2390390afa36
grog25 <73005196+grog25@users.noreply.github.com>
2021-03-30 22:01:48 -0600

    Added partial database. Added partial RecipeModel. Adjusted the activity_add_recipe to reflect the different parts of the recipe. Split database into two tables. Will still learning how to join the tables together. Measurements class might get deleted if I can do the equations in the AddRecipe class. Planning on getting rid of the grocery list just for the class but will bring it back for our app's sake.  Greg

--- app/src/main/java/com/example/mymenu/AddRecipe.java
+++ app/src/main/java/com/example/mymenu/AddRecipe.java
@@ -8,52 +8,151 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddRecipe extends AppCompatActivity {
+public class AddRecipe extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-    RecyclerView recyclerView;
-    CheckBox cb_Breakfast, cb_Lunch, cb_Dinner, cb_Dessert, cb_SideDish, cb_Other;
+    Button btn_save, btn_addIngredient;
+    EditText et_recipeName, et_mNumber, et_ingredient, et_instructions;
+    ListView lv_ingredients;
 
+    private static final String[] mealType = {"Breakfast", "Lunch", "Dinner", "Desert", "Side Dish", "Other"};
+    private static final String[] mUnits = {"pinch", "dash", "cups", "tsp", "tbsp", "ounces", "lbs", "ml", "liters"};
+    private static final String[] fractions = {"1/8", "1/4", "1/2", "2/3", "3/4",};
+
+    ArrayAdapter recipeArrayAdapter;
+    DataBaseHelper dataBaseHelper;
 
     private static final String TAG = "AddRecipe";
     List<String> ingredient = new ArrayList<String>();
     List<String> measurement = new ArrayList<String>();
+    List<String> recipes = new ArrayList<String>();
     Recipe recipe = new Recipe();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
-        recyclerView = findViewById(R.id.recyclerView);
-        cb_Breakfast = findViewById(R.id.cb_Breakfast);
-        cb_Lunch = findViewById(R.id.cb_Lunch);
-        cb_Dinner = findViewById(R.id.cb_Dinner);
-        cb_Dessert = findViewById(R.id.cb_Dessert);
-        cb_SideDish = findViewById(R.id.cb_SideDish);
-        cb_Other = findViewById(R.id.cb_Other);
 
-        ingredient.add("Ingredients");
-        measurement.add("Measurements");
+        et_ingredient = findViewById(R.id.et_ingredient);
+        et_mNumber = findViewById(R.id.et_mNumber);
+        et_recipeName = findViewById(R.id.et_recipeName);
+        et_instructions = findViewById(R.id.et_instructions);
+        btn_addIngredient = findViewById(R.id.btn_addIngredient);
+        btn_save = findViewById(R.id.btn_save);
+        lv_ingredients = findViewById(R.id.lv_ingredients);
 
-        recyclerView = findViewById(R.id.displayRecipe);
+        Spinner sp_fraction = findViewById(R.id.sp_fraction);
+        ArrayAdapter<String>adapter1 = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, fractions);
+
+        Spinner sp_mUnit = findViewById(R.id.mealSelector);
+        ArrayAdapter<String>adapter2 = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, mUnits);
+
+        Spinner sp_mealType = findViewById(R.id.sp_mealType);
+        ArrayAdapter<String>adapter3 = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, mealType);
+
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_fraction.setAdapter(adapter1);
+        sp_fraction.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_mUnit.setAdapter(adapter2);
+        sp_mUnit.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_mealType.setAdapter(adapter3);
+        sp_mealType.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+
+        //ingredient.add("Ingredients");
+        //measurement.add("Measurements");
+
+        dataBaseHelper = new DataBaseHelper(AddRecipe.this);
+
+        ShowIngredientsOnListView(dataBaseHelper);
+
+        // button listeners for the add and view all buttons
+        btn_save.setOnClickListener(v -> {
+
+            RecipeModel recipeModel;
+
+            try {
+                recipeModel = new RecipeModel(1, et_recipeName.getText().toString(),
+                        et_measurement.getText().toString(), et_ingredient.getText().toString(), cb_Dinner.isChecked());
+                Toast.makeText(AddRecipe.this, recipeModel.toString(), Toast.LENGTH_SHORT).show();
+            }
+            catch (Exception e){
+                Toast.makeText(AddRecipe.this, "Error creating recipe", Toast.LENGTH_SHORT).show();
+                recipeModel = new RecipeModel(-1, "error", "error", "error",false);
+            }
+
+            DataBaseHelper dataBaseHelper = new DataBaseHelper(AddRecipe.this);
+
+            boolean success = dataBaseHelper.addRecipe(RecipeModel);
+
+            Toast.makeText(AddRecipe.this, "Success " + success, Toast.LENGTH_SHORT).show();
+
+        });
+
+        btn_addIngredient.setOnClickListener(v -> {
+
+            dataBaseHelper = new DataBaseHelper(AddRecipe.this);
+
+            ShowCustomersOnListView(dataBaseHelper);
+
+            //Toast.makeText(MainActivity.this, everyone.toString(), Toast.LENGTH_SHORT).show();
+
+        });
+
+        lv_ingredients.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Ingredients clickedIngredient = (Ingredients) parent.getItemAtPosition(position);
+                dataBaseHelper.editOne(clickedIngredient);
+                ShowIngredientsOnListView(dataBaseHelper);
+                Toast.makeText(AddRecipe.this, "Deleted " + clickedIngredient.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    private void ShowIngredientsOnListView(DataBaseHelper dataBaseHelper2) {
+        recipeArrayAdapter = new ArrayAdapter<Ingredients>
+                (AddRecipe.this, android.R.layout.simple_list_item_1, dataBaseHelper2.getEveryone());
+        lv_ingredients.setAdapter(recipeArrayAdapter);
     }
 
 
     public void addIngredient(View view){
         Log.d(TAG, "add ingredient function called");
 
-        EditText enterIngredient = (EditText) findViewById(R.id.enterIngredient);
+        EditText enterIngredient = (EditText) findViewById(R.id.et_ingredient);
         ingredient.add(enterIngredient.getText().toString());
         enterIngredient.setText("");
 
-        EditText enterMeasurement = (EditText) findViewById(R.id.measurement);
+        EditText enterMeasurement = (EditText) findViewById(R.id.et_mNumber);
         measurement.add(enterMeasurement.getText().toString());
         enterMeasurement.setText("");
 
@@ -64,7 +163,7 @@ public class AddRecipe extends AppCompatActivity {
     }
 
     public void saveRecipe() {
-        EditText recipeName = (EditText) findViewById(R.id.recipeName);
+        EditText recipeName = (EditText) findViewById(R.id.et_recipeName);
         recipe.setRecipeName(recipeName.getText().toString());
     }
 
@@ -72,5 +171,6 @@ public class AddRecipe extends AppCompatActivity {
         Intent intent = new Intent(this, activity_recipe_instructions.class);
         startActivity(intent);
     }
+
 
 }
